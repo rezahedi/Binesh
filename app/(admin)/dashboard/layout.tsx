a6557f728915409419e98794deb68d6474700c2b
Reza Zahedi <r.zahedi@gmail.com>
2024-05-17 14:25:29 -0700

    just prettified

--- app/(admin)/dashboard/layout.tsx
+++ app/(admin)/dashboard/layout.tsx
@@ -55,136 +55,136 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Toaster />
-          <div id='sidebarNav' className="group isOpen flex flex-row min-h-screen w-full bg-muted/40">
-            <Navbar />
-            <div className="flex-1 flex flex-col sm:gap-4 sm:py-4">
-              <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
-                <Sheet>
-                  <SheetTrigger asChild>
-                    <Button size="icon" variant="outline" className="md:hidden">
-                      <PanelLeft className="size-5" />
-                      <span className="sr-only">Toggle Menu</span>
-                    </Button>
-                  </SheetTrigger>
-                  <SheetContent side="left" className="md:max-w-xs">
-                    <nav className="grid gap-6 text-lg font-medium">
-                      <Link
-                        href="#"
-                        className="flex h-10 shrink-0 items-center px-2.5 gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:text-base"
-                      >
-                        <Package2 className="size-5" />
-                        <span className="">Binesh</span>
-                      </Link>
-                      <Link
-                        href="#"
-                        className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-                      >
-                        <Home className="size-5" />
-                        Dashboard
-                      </Link>
-                      <Link
-                        href="#"
-                        className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-                      >
-                        <ShoppingCart className="size-5" />
-                        Courses
-                      </Link>
-                      <Link
-                        href="#"
-                        className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-                      >
-                        <Package className="size-5" />
-                        Categories
-                      </Link>
-                      <Link
-                        href="#"
-                        className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-                      >
-                        <Package className="size-5" />
-                        Learning Paths
-                      </Link>
-                      <Link
-                        href="#"
-                        className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-                      >
-                        <LineChart className="size-5" />
-                        Analytics
-                      </Link>
-                      <Link
-                        href="#"
-                        className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-                      >
-                        <Users2 className="size-5" />
-                        Users
-                      </Link>
-                      <Link
-                        href="#"
-                        className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-                      >
-                        <LineChart className="size-5" />
-                        Settings
-                      </Link>
-                    </nav>
-                  </SheetContent>
-                </Sheet>
-                <Breadcrumb className="hidden md:flex">
-                  <BreadcrumbList>
-                    <BreadcrumbItem>
-                      <BreadcrumbLink asChild>
-                        <Link href={`${process.env.NEXT_ADMIN_BASE}`}>Dashboard</Link>
-                      </BreadcrumbLink>
-                    </BreadcrumbItem>
-                    <BreadcrumbSeparator />
-                    <BreadcrumbItem>
-                      <BreadcrumbLink asChild>
-                        <Link href={`${process.env.NEXT_ADMIN_BASE}/courses`}>Courses</Link>
-                      </BreadcrumbLink>
-                    </BreadcrumbItem>
-                    <BreadcrumbSeparator />
-                    <BreadcrumbItem>
-                      <BreadcrumbPage>All Courses</BreadcrumbPage>
-                    </BreadcrumbItem>
-                  </BreadcrumbList>
-                </Breadcrumb>
-                <div className="relative ml-auto flex-1 md:grow-0">
-                  <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-                  <Input
-                    type="search"
-                    placeholder="Search..."
-                    className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"
-                  />
-                </div>
-                <DropdownMenu>
-                  <DropdownMenuTrigger asChild>
-                    <Button
-                      variant="outline"
-                      size="icon"
-                      className="overflow-hidden rounded-full"
+        <div id='sidebarNav' className="group isOpen flex flex-row min-h-screen w-full bg-muted/40">
+          <Navbar />
+          <div className="flex-1 flex flex-col sm:gap-4 sm:py-4">
+            <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
+              <Sheet>
+                <SheetTrigger asChild>
+                  <Button size="icon" variant="outline" className="md:hidden">
+                    <PanelLeft className="size-5" />
+                    <span className="sr-only">Toggle Menu</span>
+                  </Button>
+                </SheetTrigger>
+                <SheetContent side="left" className="md:max-w-xs">
+                  <nav className="grid gap-6 text-lg font-medium">
+                    <Link
+                      href="#"
+                      className="flex h-10 shrink-0 items-center px-2.5 gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:text-base"
                     >
-                      <Image
-                        src="/placeholder-user.jpg"
-                        width={36}
-                        height={36}
-                        alt="Avatar"
-                        className="overflow-hidden"
-                      />
-                    </Button>
-                  </DropdownMenuTrigger>
-                  <DropdownMenuContent align="end">
-                    <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuItem>Settings</DropdownMenuItem>
-                    <DropdownMenuItem>Support</DropdownMenuItem>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuItem>Logout</DropdownMenuItem>
-                  </DropdownMenuContent>
-                </DropdownMenu>
-              </header>
-              <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
-                {children}
-              </main>
-            </div>
+                      <Package2 className="size-5" />
+                      <span className="">Binesh</span>
+                    </Link>
+                    <Link
+                      href="#"
+                      className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+                    >
+                      <Home className="size-5" />
+                      Dashboard
+                    </Link>
+                    <Link
+                      href="#"
+                      className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+                    >
+                      <ShoppingCart className="size-5" />
+                      Courses
+                    </Link>
+                    <Link
+                      href="#"
+                      className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+                    >
+                      <Package className="size-5" />
+                      Categories
+                    </Link>
+                    <Link
+                      href="#"
+                      className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+                    >
+                      <Package className="size-5" />
+                      Learning Paths
+                    </Link>
+                    <Link
+                      href="#"
+                      className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+                    >
+                      <LineChart className="size-5" />
+                      Analytics
+                    </Link>
+                    <Link
+                      href="#"
+                      className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+                    >
+                      <Users2 className="size-5" />
+                      Users
+                    </Link>
+                    <Link
+                      href="#"
+                      className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+                    >
+                      <LineChart className="size-5" />
+                      Settings
+                    </Link>
+                  </nav>
+                </SheetContent>
+              </Sheet>
+              <Breadcrumb className="hidden md:flex">
+                <BreadcrumbList>
+                  <BreadcrumbItem>
+                    <BreadcrumbLink asChild>
+                      <Link href={`${process.env.NEXT_ADMIN_BASE}`}>Dashboard</Link>
+                    </BreadcrumbLink>
+                  </BreadcrumbItem>
+                  <BreadcrumbSeparator />
+                  <BreadcrumbItem>
+                    <BreadcrumbLink asChild>
+                      <Link href={`${process.env.NEXT_ADMIN_BASE}/courses`}>Courses</Link>
+                    </BreadcrumbLink>
+                  </BreadcrumbItem>
+                  <BreadcrumbSeparator />
+                  <BreadcrumbItem>
+                    <BreadcrumbPage>All Courses</BreadcrumbPage>
+                  </BreadcrumbItem>
+                </BreadcrumbList>
+              </Breadcrumb>
+              <div className="relative ml-auto flex-1 md:grow-0">
+                <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+                <Input
+                  type="search"
+                  placeholder="Search..."
+                  className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"
+                />
+              </div>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button
+                    variant="outline"
+                    size="icon"
+                    className="overflow-hidden rounded-full"
+                  >
+                    <Image
+                      src="/placeholder-user.jpg"
+                      width={36}
+                      height={36}
+                      alt="Avatar"
+                      className="overflow-hidden"
+                    />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                  <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>Settings</DropdownMenuItem>
+                  <DropdownMenuItem>Support</DropdownMenuItem>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>Logout</DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </header>
+            <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
+              {children}
+            </main>
           </div>
+        </div>
       </body>
     </html>
   );
