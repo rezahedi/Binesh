7fb8f77ba2597f638f1607f32f5c80435c1587f7
Reza Zahedi <r.zahedi@gmail.com>
2024-05-18 10:43:44 -0700

    Create categories endpoint

---
+++ app/api/admin/categories/route.ts
@@ -0,0 +1,61 @@
+import { PrismaClient } from "@prisma/client"
+import { withAdmin } from "@/lib/auth";
+import { NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+export const GET = withAdmin(async ({ searchParams }) => {
+  const ROWS_PER_PAGE = 100
+  const {
+    search,
+    sort = "name",
+    page,
+  } = searchParams as {
+    search?: string;
+    sort?: "name" | "description";
+    page?: string;
+  };
+
+  const response = await prisma.categories.findMany(
+    {
+      where: {
+        ...(search && {
+          OR: [
+            {
+              name: { contains: search },
+            },
+            {
+              description: { contains: search },
+            },
+          ],
+        }),
+      },
+      orderBy: {
+        [sort]: "asc",
+      },
+      take: ROWS_PER_PAGE,
+      ...(page && {
+        skip: (parseInt(page) - 1) * ROWS_PER_PAGE,
+      }),
+    }
+  );
+
+  return NextResponse.json(response);
+});
+
+// TODO: This endpoint not checked yet
+
+export const POST = withAdmin(async ({ req }) => {
+  const { name, slug, image, description } = await req.json();
+
+  const response = await prisma.categories.create({
+    data: {
+      name,
+      slug,
+      image,
+      description,
+    },
+  });
+
+  return NextResponse.json(response);
+});
