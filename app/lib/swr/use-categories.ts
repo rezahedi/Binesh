af0dbad27485bffd75643e5b1339dae1fd9c3d1a
Reza Zahedi <r.zahedi@gmail.com>
2024-05-20 14:14:06 -0700

    Create useCategory hook

---
+++ app/lib/swr/use-categories.ts
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import { CategoryProps } from "@/lib/types";
+import { fetcher } from "@/utils/fetcher";
+import useSWR from "swr";
+
+export default function useCategories() {
+
+  const [admin, setAdmin] = useState(false);
+  useEffect(() => {
+    if (window.location.pathname.startsWith("/dashboard")) {
+      setAdmin(true);
+    }
+  }, []);
+
+  const { data: categories, isValidating } = useSWR<CategoryProps[]>(
+    admin
+      ? `/api/admin/categories`
+      : `/api/categories`,
+    fetcher,
+    {
+      dedupingInterval: 30000,
+    },
+  );
+
+  return {
+    categories,
+    loading: categories ? false : true,
+    isValidating,
+  };
+}
