2dd0df24dadd12b0e2edf0fcdc16348485cb256b
Reza Zahedi <r.zahedi@gmail.com>
2024-05-20 21:46:53 -0700

    Create courses page

--- app/(application)/learn/courses/page.tsx
+++ app/(application)/learn/courses/page.tsx
@@ -1,8 +1,21 @@
-export default function Page() {
+import { CourseProps } from '@/lib/types'
+import Link from 'next/link'
+
+export default async function Page() {
+  
+  // fetch courses from /api/admin/courses
+  let courses: CourseProps[] = []
+  await fetch('http://localhost:3000/api/admin/courses')
+    .then(async res => {
+      courses = await res.json()
+    })
 
   return (
     <div className="container">
       <h3 className="font-semibold text-xl">Courses</h3>
+      {courses.map((course) => (
+        <div key={course.id}><Link href={`./courses/${course.slug}`}>{course.name}</Link></div>
+      ))}
     </div>
   )
 }
