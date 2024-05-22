88ccf1a9f6396c8bf27e2777000913682f9f6c79
Reza Zahedi <r.zahedi@gmail.com>
2024-05-22 13:52:39 -0700

    Add style & position to connectors

--- app/(application)/learn/courses/[courseSlug]/page.tsx
+++ app/(application)/learn/courses/[courseSlug]/page.tsx
@@ -68,21 +68,23 @@ export default async function page(
         <b>Lessons:</b>
         <div className='flex flex-col'>
           {lessons.map((lesson, index) => (
-            <div key={lesson.id} className={`p-8 w-fit ${treeClasses[index%4]} flex flex-col gap-3 items-center`}>
-              <button className="group rotate-45 relative inline-flex size-12 items-center justify-center overflow-hidden rounded-full border border-[#a30036] p-6 font-medium text-[#a30036] transition-all duration-100 shadow-[5px_5px_#a30036] hover:translate-y-[3px] hover:shadow-[3px_3px_#a30036] active:translate-y-[7px] active:shadow-[0px_0px_#a30036]">
-                <span className='-rotate-45'>
-                  <Footprints />
+            <div key={lesson.id} className={`p-8 w-fit ${treeClasses[index%4]} relative h-36 w-36`}>
+              <div className='absolute flex flex-col gap-3 items-center'>
+                <button className="group rotate-45 relative inline-flex size-12 items-center justify-center overflow-hidden rounded-full border border-[#a30036] p-6 font-medium text-[#a30036] transition-all duration-100 shadow-[5px_5px_#a30036] hover:translate-y-[3px] hover:shadow-[3px_3px_#a30036] active:translate-y-[7px] active:shadow-[0px_0px_#a30036]">
+                  <span className='-rotate-45'>
+                    <Footprints />
+                  </span>
+                </button>
+                <span className='text-sm w-36 text-balance text-center'>
+                  {lesson.name}
                 </span>
-              </button>
-              <span className='text-sm max-w-36 text-balance text-center'>
-                {lesson.name}
-              </span>
+              </div>
               {/* <a href={`./${params.courseSlug}/${lesson.slug}`}>{lesson.name}</a>
               <p>{lesson.description}</p> */}
-              {index%4 === 0 && <CenterLeft />}
-              {index%4 === 1 && <LeftCenter />}
-              {index%4 === 2 && <CenterRight />}
-              {index%4 === 3 && <RightCenter />}
+              {index%4 === 0 && <CenterLeft className='absolute top-[77px] left-[-110px] w-[454px] text-[#e5e5e5]' />}
+              {index%4 === 1 && <LeftCenter className='absolute top-[77px] left-[-52px] w-[250px] text-[#e5e5e5]' />}
+              {index%4 === 2 && <CenterRight className='absolute top-[-97px] left-[113px] w-[250px] text-[#e5e5e5]' />}
+              {index%4 === 3 && <RightCenter className='absolute top-[77px] left-[-244px] w-[450px] text-[#e5e5e5]' />}
             </div>
           ))}
         </div>
