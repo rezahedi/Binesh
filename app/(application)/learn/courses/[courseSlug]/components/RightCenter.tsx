fe3d2aeafa79e57ec21163c5d9650583ee989e92
Reza Zahedi <r.zahedi@gmail.com>
2024-05-22 13:52:12 -0700

    Add class prop to components

--- app/(application)/learn/courses/[courseSlug]/components/RightCenter.tsx
+++ app/(application)/learn/courses/[courseSlug]/components/RightCenter.tsx
@@ -1,6 +1,9 @@
-export default function RightCenter() {
+export default function RightCenter(
+  { className }:
+  { className?: string }
+) {
   return (
-    <svg viewBox="0 0 454 126" fill="none">
+    <svg viewBox="0 0 454 126" fill="none" className={className}>
       <path d="M331 1.5L179.357 90.5601C176.728 92.1041 176.724 95.9035 179.349 97.4536L226 125" stroke="currentColor" stroke-width="4"></path>
     </svg>
   )
