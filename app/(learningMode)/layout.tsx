9137cfe63dce161504bdd011568477f94ef4b253
Reza Zahedi <r.zahedi@gmail.com>
2024-05-27 12:41:21 -0700

    Remove title from learningmode layout

--- app/(learningMode)/layout.tsx
+++ app/(learningMode)/layout.tsx
@@ -16,7 +16,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <h2>Learning mode layout</h2>
         {children}
       </body>
     </html>
