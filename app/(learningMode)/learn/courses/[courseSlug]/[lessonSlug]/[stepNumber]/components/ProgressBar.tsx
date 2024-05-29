5073abce5e76f55b09104d39171d39898709bb6b
Reza Zahedi <r.zahedi@gmail.com>
2024-05-29 14:40:25 -0700

    Create progressbar component

---
+++ app/(learningMode)/learn/courses/[courseSlug]/[lessonSlug]/[stepNumber]/components/ProgressBar.tsx
@@ -0,0 +1,23 @@
+
+export default function ProgressBar(
+  {
+    progress=0,
+    focused=false
+  }:
+  {
+    progress: number,
+    focused?: boolean
+  }
+) {
+  if (progress<0) progress = 0;
+  if (progress>100) progress = 100;
+  return (
+    <div className="group cursor-pointer py-3 w-full">
+      <div className={`border border-${focused ? 'gray-300' : 'transparent'} rounded-full p-0.5`}>
+        <div className="w-full bg-gray-200 rounded-full h-2 group-hover:bg-gray-300">
+          <div className="bg-[#29cc57] h-full rounded-full group-hover:bg-[#179e7e]" style={{width: `${progress}%`}}></div>
+        </div>
+      </div>
+    </div>
+  )
+}
