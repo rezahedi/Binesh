ae017d0008ee5ef1f8df88dec8c385b8f9230cd5
Reza Zahedi <r.zahedi@gmail.com>
2024-06-06 10:17:23 -0700

    Import parts dynamically

--- app/(learningMode)/learn/courses/[courseSlug]/[lessonSlug]/[stepNumber]/page.tsx
+++ app/(learningMode)/learn/courses/[courseSlug]/[lessonSlug]/[stepNumber]/page.tsx
@@ -1,13 +1,13 @@
 'use client';
 
-import { useState, useEffect, Suspense, lazy } from 'react'
-import steps from '@contents/computer-science/beginners-python-programming/welcome-to-python';
+import { useState, useEffect } from 'react'
 import ShowStep from './components/ShowStep';
 
-// Utility function to dynamically import a component
-const dynamicImport = (path: string) => {
-  return lazy(() => import(`${path}`));
-};
+type Step = {
+  title: string,
+  content: () => React.ReactNode,
+  answer: number,
+}
 
 export default function Page(
   { params }:
@@ -15,33 +15,36 @@ export default function Page(
 ) {
 
   const [currentStep, setCurrentStep] = useState<number>(0);
-  const [parts, setParts] = useState<typeof steps[0][]>([]);
+  const [parts, setParts] = useState<Step[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
-  // const LazyComponent = lazy(() => import('@contents/Part1'));
-  // const LazyComponent = lazy(() => import('@contents/computer-science/beginners-python-programming/welcome-to-python'));
+  const [allParts, setAllParts] = useState<Step[]>([]);
 
   useEffect(() => {
     (async () => {
       setLoading(true);
   
       // TODO: Fetch all the parts
+      const { default: steps } = await import('@contents/computer-science/beginners-python-programming/welcome-to-python');
+      setAllParts(steps);
 
-      // Timeout to simulate loading
-      await new Promise(resolve => setTimeout(resolve, 2000));
-      // Set the first part
-      setParts([
-        steps[0],
-      ]);
-  
       setLoading(false);
     })();
   }, []);
 
   useEffect(() => {
-    if (currentStep > 0 && currentStep < steps.length) {
+    if (loading) return;
+
+    // Set the first part
+    setParts([
+      allParts[0],
+    ]);
+  }, [loading]);
+
+  useEffect(() => {
+    if (currentStep > 0 && currentStep < allParts.length) {
       setParts([
         ...parts,
-        steps[currentStep],
+        allParts[currentStep],
       ]);
     }
   }, [currentStep]);
@@ -54,7 +57,7 @@ export default function Page(
   }, [parts]);
 
   const checkAnswer = (answer: number): boolean => {
-    if (answer !== steps[currentStep].answer) {
+    if (answer !== allParts[currentStep].answer) {
       return false;
     }
     return true;
@@ -77,7 +80,7 @@ export default function Page(
               <ShowStep key={index} {...step} checkAnswer={checkAnswer} />
             ))}
           </div>
-          {parseInt(params.stepNumber) < steps.length &&
+          {parseInt(params.stepNumber) < allParts.length &&
             <button className='bg-blue-500 text-white px-4 py-2 rounded' onClick={()=>setCurrentStep(currentStep+1)}>Continue</button>
           }
         </>
