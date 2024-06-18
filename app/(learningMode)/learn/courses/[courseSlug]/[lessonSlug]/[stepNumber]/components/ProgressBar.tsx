17d47d930e2012e0aa85d458f9762172661301da
Reza Zahedi <r.zahedi@gmail.com>
2024-06-18 12:30:48 -0700

    add css transition to progressbar changes

--- app/(learningMode)/learn/courses/[courseSlug]/[lessonSlug]/[stepNumber]/components/ProgressBar.tsx
+++ app/(learningMode)/learn/courses/[courseSlug]/[lessonSlug]/[stepNumber]/components/ProgressBar.tsx
@@ -17,7 +17,7 @@ export default function ProgressBar(
     <div className="group cursor-pointer py-3 w-full" title={title}>
       <div className={`border border-${focused ? 'gray-300' : 'transparent'} rounded-full p-0.5`}>
         <div className="w-full bg-gray-200 rounded-full h-2 group-hover:bg-gray-300">
-          <div className="bg-[#29cc57] h-full rounded-full group-hover:bg-[#179e7e]" style={{width: `${progress}%`}}></div>
+          <div className="bg-[#29cc57] h-full rounded-full group-hover:bg-[#179e7e] transition-all duration-200" style={{width: `${progress}%`}}></div>
         </div>
       </div>
     </div>
