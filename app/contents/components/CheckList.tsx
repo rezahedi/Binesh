b32073cc2d96afdb75da95cec74fd7e21ec8d401
Reza Zahedi <r.zahedi@gmail.com>
2024-09-09 17:00:41 -0700

    Create CheckList component

---
+++ app/contents/components/CheckList.tsx
@@ -0,0 +1,34 @@
+import { cn } from '@/utils/cn';
+
+export default function CheckList({
+  list,
+  className='',
+  onChange,
+}: {
+  list: string[];
+  className?: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}) {
+
+  return (
+    <div className='group flex flex-col justify-center'>
+      {list.map((option, index) => (
+        <label
+          key={index}
+          className='cursor-pointer border-2 border-transparent rounded-xl px-2 py-1 has-[:checked]:border-green-500 focus-within:has-[:checked]:border-black focus-within:border-black'
+        >
+          <input
+            name='checkList'
+            className={cn(className, 'outline-none mr-2')}
+            type='checkbox'
+            value={index+1}
+            onFocus={(e)=>onChange(e)}
+          />
+          <span className='ml-2'>
+            {option}
+          </span>
+        </label>
+      ))}
+    </div>
+  )
+}
