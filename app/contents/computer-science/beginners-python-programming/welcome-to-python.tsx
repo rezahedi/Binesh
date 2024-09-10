61b5d0a4eeaf215a890568370a5912779fd85c2c
Reza Zahedi <r.zahedi@gmail.com>
2024-09-09 17:07:40 -0700

    Add image and use checklist

--- app/contents/computer-science/beginners-python-programming/welcome-to-python.tsx
+++ app/contents/computer-science/beginners-python-programming/welcome-to-python.tsx
@@ -1,5 +1,7 @@
 import RadioList from "@/contents/components/RadioList";
+import CheckList from "@/contents/components/CheckList";
 import { Part } from "@/lib/types";
+import Image from "next/image";
 
 const parts: Part[] = [
   {
@@ -14,6 +16,7 @@ const parts: Part[] = [
           <>
             <h2 className="font-bold text-2xl py-2">How to plus two numbers?</h2>
             <p className="py-2">In Math, we can add two numbers together by using the plus sign (+). For example, 1 + 2 = 3.</p>
+            <Image className="my-2 mx-auto" src="/Screenshot-2024-06-10-095546.png" alt="Plus two numbers" width={300} height={300} />
             <div className="bg-gray-200 p-2 rounded-md p-4 my-2">
               <p>What is the result of below equation?</p> 
               2 + 2 = <input type="number" className="rounded-md w-20 border-2 border-green-500 mx-2 p-2" autoFocus onChange={(e) => setAnswer(e.target.valueAsNumber)} /> 
@@ -22,7 +25,7 @@ const parts: Part[] = [
       },
       {
         title: 'Step 2',
-        answer: 2,
+        answer: 4,
         content: (
           { setAnswer }: { setAnswer: (answer: number | undefined) => void }
         ) => 
@@ -31,13 +34,21 @@ const parts: Part[] = [
             <p className="py-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit voluptatum aperiam labore ab est commodi optio, dolores nihil! Iusto recusandae aspernatur adipisci libero enim corrupti ad sed alias deleniti laboriosam!</p>
             <div className="bg-gray-200 p-2 rounded-md p-4 my-2">
               <p>What's the total weight on the scale?</p> 
-              <RadioList list={['c + c + c + c', '4c', '4 . c', 'All of the above']} />
+              <RadioList
+                list={[
+                  'c + c + c + c',
+                  '4c',
+                  '4 . c',
+                  'All of the above'
+                ]}
+                onChange={(e) => setAnswer(parseInt(e.target.value))}
+              />
             </div>
           </>
       }, 
       {
         title: 'Step 3',
-        answer: 9,
+        answer: 3,
         content: (
           { setAnswer }: { setAnswer: (answer: number | undefined) => void }
         ) => 
@@ -45,8 +56,8 @@ const parts: Part[] = [
             <h2 className="font-bold text-2xl py-2">How to multiply two numbers?</h2>
             <p className="py-2">In Math, we can multiply two numbers together by using the multiplication sign (×). For example, 2 × 3 = 6.</p>
             <div className="bg-gray-200 p-2 rounded-md p-4 my-2">
-              <p>What is the result of below equation?</p> 
-              3 × 3 = <input type="number" className="rounded-md w-20 border-2 border-green-500 mx-2 p-2" autoFocus onChange={(e) => setAnswer(e.target.valueAsNumber)} />
+              <p>What is the result of below equation?</p>
+              <CheckList list={['First option', 'Second option', 'Third option', 'Last option']} onChange={(e) => setAnswer(parseInt(e.target.value))} />
             </div>
           </>
       }
