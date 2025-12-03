9a4b53c3ad24d31090c71e4564f4ae7699b8c708
Reza Zahedi <r.zahedi@gmail.com>
2025-12-03 12:57:19 -0800

    create MathField component

---
+++ app/lib/MathLive.tsx
@@ -0,0 +1,40 @@
+import { useRef, HTMLAttributes } from "react";
+import "mathlive";
+
+export interface IMathFieldProps {
+  value: string;
+  options?: Partial<MathfieldOptions>;
+  onChange?: (value: string) => void;
+  style?: HTMLAttributes<HTMLDivElement>["style"];
+}
+
+MathfieldElement.locale = "en";
+MathfieldElement.decimalSeparator = ",";
+MathfieldElement.keypressSound = "none";
+MathfieldElement.plonkSound = "none";
+
+export default function MathField(props: IMathFieldProps) {
+  const mathField = useRef<MathfieldElement>(null);
+  const onInput = () => props.onChange?.(mathField.current?.getValue() || "");
+  const { options, style } = props;
+  const divStyle = {
+    ...style,
+    width: "200px",
+    fontSize: "1.5rem",
+  };
+
+  const init = (mf: MathfieldElement | null) => {
+    if (mf) {
+      mathField.current = mf;
+    }
+  };
+
+  return (
+    <math-field
+      ref={init}
+      onInput={onInput}
+      options={options}
+      style={divStyle}
+    />
+  );
+}
