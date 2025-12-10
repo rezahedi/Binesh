04a41589f806bec7bc0ddcb8098d7a6eed1a9032
Reza Zahedi <r.zahedi@gmail.com>
2025-12-10 12:16:35 -0800

    add user auth condition to main page in learning mode

--- app/(learningMode)/learn/courses/[courseSlug]/[lessonSlug]/page.tsx
+++ app/(learningMode)/learn/courses/[courseSlug]/[lessonSlug]/page.tsx
@@ -2,8 +2,14 @@
 
 import { ProgressProvider } from "./ProgressContext";
 import Content from "./components/Content";
+import { useUser } from "@stackframe/stack";
+import { useRouter } from "next/navigation";
 
 export default function Page() {
+  const router = useRouter();
+  const user = useUser();
+  if (!user) return router.push("/");
+
   return (
     <ProgressProvider>
       <Content />
