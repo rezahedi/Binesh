9a5d76e90d56b545119e157517310269394648fb
Reza Zahedi <r.zahedi@gmail.com>
2025-12-12 20:19:20 -0800

    install dotenv pkg

--- app/db/index.ts
+++ app/db/index.ts
@@ -1,5 +1,6 @@
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
+import "dotenv/config";
 import assert from "node:assert";
 import * as schema from "@/db/schema";
 
