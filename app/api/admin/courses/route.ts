7a8d8f5e4ab191ac578502a5d679c1ce6a6cbae7
Reza Zahedi <r.zahedi@gmail.com>
2025-12-15 12:15:39 -0800

    remove progress from admin route

--- app/api/admin/courses/route.ts
+++ app/api/admin/courses/route.ts
@@ -1,9 +1,8 @@
 import { NextResponse } from "next/server";
-import { and, desc, eq, getTableColumns, like, or } from "drizzle-orm";
+import { desc, eq, getTableColumns, like, or } from "drizzle-orm";
 import db from "@/db";
-import { courses, categories, courseProgress } from "@/db/schema";
+import { courses, categories } from "@/db/schema";
 import { getSearchParams } from "@/utils/urls";
-import { stackServerApp } from "@stack/server";
 import { withAdmin } from "@/lib/auth";
 import { CourseProps } from "@/lib/types";
 
@@ -20,30 +19,16 @@ export const GET = withAdmin(async ({ req }: { req: Request }) => {
     page?: string;
   };
 
-  const user = await stackServerApp.getUser();
-
-  if (!user) {
-    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
-  }
-
   const query = db
     .select({
       ...getTableColumns(courses),
       category: getTableColumns(categories),
-      progress: getTableColumns(courseProgress),
     })
     .from(courses)
     .offset((Number(page) - 1) * ROWS_PER_PAGE)
     .limit(ROWS_PER_PAGE)
     .orderBy(desc(courses[sort]))
-    .leftJoin(categories, eq(courses.categoryID, categories.id))
-    .leftJoin(
-      courseProgress,
-      and(
-        eq(courseProgress.userID, user.id),
-        eq(courseProgress.courseID, courses.id)
-      )
-    );
+    .leftJoin(categories, eq(courses.categoryID, categories.id));
 
   if (search)
     query.where(
