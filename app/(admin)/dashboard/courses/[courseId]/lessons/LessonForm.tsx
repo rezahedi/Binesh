c5350aeeaccb28df8e4381110cf2ddbb2e126142
Reza Zahedi <r.zahedi@gmail.com>
2025-12-16 21:09:56 -0800

    add slug uniqueness in lesson form

--- app/(admin)/dashboard/courses/[courseId]/lessons/LessonForm.tsx
+++ app/(admin)/dashboard/courses/[courseId]/lessons/LessonForm.tsx
@@ -10,17 +10,69 @@ import {
 import { Textarea } from "@/components/ui/textarea";
 import { STATUS_VALUES } from "@/db/schema";
 import { LessonsProps } from "@/lib/types";
+import { cn } from "@/utils/cn";
+import { useParams } from "next/navigation";
+import { useState } from "react";
 
 const LessonForm = ({ lesson }: { lesson?: LessonsProps | null }) => {
+  const [slug, setSlug] = useState<string>(lesson?.slug || "");
+  const [isSlugUnique, setIsSlugUnique] = useState<boolean | null>(null);
+  const { courseId } = useParams();
+
+  const createSlug = (str: string) => {
+    return str
+      .toLowerCase()
+      .replace(/ /g, "-")
+      .replace(/[^\w-]+/g, "");
+  };
+
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (lesson) return;
+    setSlug(createSlug(e.target.value));
+  };
+
+  const handleCheckSlug = async (slug: string) => {
+    setIsSlugUnique(null);
+    if (slug === "" || (lesson && lesson.slug === slug)) return;
+
+    const res = await fetch(
+      `/api/admin/courses/${courseId}/lessons/slug/${slug}`
+    ).then((body) => body.text());
+    setIsSlugUnique(Boolean(Number(res)));
+  };
+
   return (
     <>
       <div>
         <Label htmlFor="name">Name *:</Label>
-        <Input id="name" name="name" required defaultValue={lesson?.name} />
+        <Input
+          id="name"
+          name="name"
+          required
+          defaultValue={lesson?.name}
+          onChange={handleNameChange}
+        />
       </div>
       <div>
         <Label htmlFor="slug">URL Slug *:</Label>
-        <Input id="slug" name="slug" required defaultValue={lesson?.slug} />
+        <Input
+          id="slug"
+          name="slug"
+          required
+          value={slug}
+          onChange={(e) => setSlug(e.target.value)}
+          onBlur={(e) => handleCheckSlug(e.target.value)}
+        />
+        {isSlugUnique !== null && (
+          <i
+            className={cn(
+              "text-sm",
+              isSlugUnique ? "text-green-500" : "text-red-500"
+            )}
+          >
+            {isSlugUnique ? "The slug is unique" : "The slug is not unique"}
+          </i>
+        )}
       </div>
       <div>
         <Label htmlFor="description">Description *:</Label>
