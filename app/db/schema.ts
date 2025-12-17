88d6c4bd235858fab5c3f7881a66cf633c3509f5
Reza Zahedi <r.zahedi@gmail.com>
2025-12-16 16:02:30 -0800

    add part column to courses table to store bite lessons

--- app/db/schema.ts
+++ app/db/schema.ts
@@ -38,6 +38,7 @@ export const courses = pgTable("courses", {
   image: text("image").notNull(),
   level: integer("level").notNull(),
   lessonsCount: integer("lessons_count").notNull().default(0),
+  part: integer("part").notNull().default(0),
   estimatedDuration: integer("estimated_duration").notNull().default(0),
   exercises: integer("exercises").notNull().default(0),
   status: statusEnum("status").notNull().default("draft"),
