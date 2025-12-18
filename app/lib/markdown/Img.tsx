d397b77a6d282d93cdc3b7fb976d215f500b0ff4
Reza Zahedi <r.zahedi@gmail.com>
2025-12-17 22:26:40 -0800

    set better aspect ratio for images

--- app/lib/markdown/Img.tsx
+++ app/lib/markdown/Img.tsx
@@ -17,8 +17,9 @@ const Img = ({
         src={String(src)}
         alt={alt || ""}
         title={title}
-        width={300}
+        width={500}
         height={300}
+        className="w-full aspect-10/8 sm:aspect-16/10 object-contain"
       />
       {title && <figcaption>{title}</figcaption>}
     </figure>
