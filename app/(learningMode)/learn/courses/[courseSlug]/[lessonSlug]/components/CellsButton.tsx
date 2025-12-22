1fa26a00ad0093909b0dec0e6342842613ea6afe
Reza Zahedi <r.zahedi@gmail.com>
2025-12-21 21:17:39 -0800

    use responsive popover

--- app/(learningMode)/learn/courses/[courseSlug]/[lessonSlug]/components/CellsButton.tsx
+++ app/(learningMode)/learn/courses/[courseSlug]/[lessonSlug]/components/CellsButton.tsx
@@ -1,19 +1,13 @@
 import { BatteryIcon } from "lucide-react";
 import { useProgress } from "@/contexts/ProgressContext";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
 import { cn } from "@/utils/cn";
-import { useState } from "react";
 import CellsStatus from "./CellsStatus";
+import ResponsivePopover from "@/components/ui/ResponsivePopover";
 
 const MAIN_BUTTON_CLASSES =
   "flex items-center gap-0.5 p-2 px-3 rounded-full hover:bg-muted cursor-pointer font-semibold text-lg";
 
 const CellsButton = ({ className }: { className?: string }) => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
   const { cells, isLoading } = useProgress();
 
   if (isLoading)
@@ -27,18 +21,18 @@ const CellsButton = ({ className }: { className?: string }) => {
   if (cells === null) return null;
 
   return (
-    <Popover open={cells === 0 || isOpen} onOpenChange={setIsOpen}>
-      <PopoverTrigger asChild>
+    <ResponsivePopover
+      title="Cells Charge"
+      button={
         <button className={cn(MAIN_BUTTON_CLASSES, className)}>
           {cells}
           <BatteryIcon className="size-5 fill-secondary/90 stroke-secondary/90 -rotate-90" />
         </button>
-      </PopoverTrigger>
-      <PopoverContent className="w-72 text-center flex flex-col gap-3 bg-background shadow-xl py-6 px-8">
-        <b className="font-semibold text-lg">Cells Charge</b>
-        <CellsStatus />
-      </PopoverContent>
-    </Popover>
+      }
+    >
+      <b className="font-semibold text-lg">Cells Charge</b>
+      <CellsStatus />
+    </ResponsivePopover>
   );
 };
 
