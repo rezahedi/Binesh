7263e6e2fe1bfcb2a79f620f64453fce2d01dd23
Reza Zahedi <r.zahedi@gmail.com>
2025-12-21 22:58:56 -0800

    use css to hide instead of returning null

--- app/(application)/components/PointsButton.tsx
+++ app/(application)/components/PointsButton.tsx
@@ -3,16 +3,20 @@
 import { GemIcon } from "lucide-react";
 import { useProgress } from "@/contexts/ProgressContext";
 import { useMediaQuery } from "@/hooks/useMediaQuery";
+import { cn } from "@/utils/cn";
 
 export default function PointsButton() {
   const { points, isLoading } = useProgress();
   const { isMobile } = useMediaQuery();
 
-  if (isMobile) return null;
-
   if (isLoading)
     return (
-      <div className="flex gap-1 items-center font-semibold text-lg">
+      <div
+        className={cn(
+          "flex gap-1 items-center font-semibold text-lg",
+          isMobile && "hidden"
+        )}
+      >
         {" "}
         <GemIcon className="animate-pulse size-5 fill-muted/90" stroke="none" />
       </div>
@@ -21,7 +25,12 @@ export default function PointsButton() {
   if (points === null) return null;
 
   return (
-    <button className="flex gap-1 items-center font-semibold text-lg">
+    <button
+      className={cn(
+        "flex gap-1 items-center font-semibold text-lg",
+        isMobile && "hidden"
+      )}
+    >
       {points.total}
       <GemIcon className="size-5 fill-primary/90" stroke="none" />
     </button>
