72b8b865c29b649e4f685400a8470ad1819c2564
Reza Zahedi <r.zahedi@gmail.com>
2025-12-24 09:59:11 -0800

    create isAdmin base func

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,4 +1,6 @@
+import db from "@/db";
 import { getSearchParams } from "@/utils/urls";
+import { stackServerApp } from "@stack/server";
 
 // Internal use only (for admin portal)
 interface WithAdminHandler {
@@ -13,6 +15,13 @@ interface WithAdminHandler {
   }): Promise<Response>;
 }
 
+export const isAdmin = async (userId: string) => {
+  // Select user info by userId and check if its an admin user then return true/false
+  // const response = await db.select().from().where(eq(users.id, userId));
+
+  return true;
+};
+
 export const withAdmin =
   (handler: WithAdminHandler) =>
   async (
@@ -21,6 +30,16 @@ export const withAdmin =
   ) => {
     // TODO: Check if user is admin
     const params = (await initialParams) || {};
+
+    const user = await stackServerApp.getUser();
+    if (!user) {
+      return new Response("Unauthorized: Login required.", { status: 401 });
+    }
+
+    const isAdminUser = await isAdmin(user.id);
+    if (!isAdminUser) {
+      return new Response("Unauthorized: Not an admin.", { status: 401 });
+    }
 
     const searchParams = getSearchParams(req.url);
     return handler({ req, params, searchParams });
