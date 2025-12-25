29767c30735ab86b319e06a395172eb04d3bf0c7
Reza Zahedi <r.zahedi@gmail.com>
2025-12-25 10:41:42 -0800

    verify webhook signature using svix

--- app/api/webhooks/stack/route.ts
+++ app/api/webhooks/stack/route.ts
@@ -2,8 +2,38 @@ import { identifyUser } from "@/(learningMode)/actions/trophy";
 import db from "@/db";
 import { usersMirror } from "@/db/schema";
 import { NextRequest, NextResponse } from "next/server";
+import { headers } from "next/headers";
+import { Webhook } from "svix";
 
 export const POST = async (request: NextRequest) => {
+  const payload = await request.text();
+  const headersList = await headers();
+
+  const svixId = headersList.get("svix-id");
+  const svixTimestamp = headersList.get("svix-timestamp");
+  const svixSignature = headersList.get("svix-signature");
+
+  if (!svixId || !svixTimestamp || !svixSignature) {
+    return new Response("Missing Svix headers", { status: 400 });
+  }
+
+  const secret = process.env.STACK_WEBHOOKS_SIGNING_SECRET;
+  if (!secret) {
+    return NextResponse.json({ message: "No secret" }, { status: 400 });
+  }
+
+  const wh = new Webhook(secret);
+  try {
+    const event = await wh.verify(payload, {
+      "svix-id": svixId,
+      "svix-timestamp": svixTimestamp,
+      "svix-signature": svixSignature,
+    });
+    console.log("event", event);
+  } catch (err) {
+    return NextResponse.json({ message: "Invalid payload" }, { status: 400 });
+  }
+
   const { type, data: user } = await request.json();
 
   if (type === "user.created") {
