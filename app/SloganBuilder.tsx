b9eb6baec1869229701f540a4db514cbad93c840
Reza Zahedi <r.zahedi@gmail.com>
2026-01-05 11:22:49 -0800

    add a quiz puzzle as hero banner

---
+++ app/SloganBuilder.tsx
@@ -0,0 +1,149 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { Button } from "./components/ui/button";
+import { cn } from "./utils/cn";
+import { useRouter } from "next/navigation";
+import { shuffle } from "./components/quizzes/SentenceBuilderQuiz";
+
+const SLOGAN = [
+  "Interactive problem",
+  "solving",
+  "that",
+  "is effective",
+  "and fun.",
+  "Excel",
+  "in math",
+  "and computer",
+  "science.",
+];
+
+type Option = { index: number; value: string };
+
+const SloganBuilder = () => {
+  const [userAnswer, setUserAnswer] = useState<Option[]>([]);
+  const [options, setOptions] = useState<Option[]>([]);
+  const [isCorrect, setIsCorrect] = useState<boolean | null>(null);
+  const router = useRouter();
+
+  const isSentenceCompleted = userAnswer.length === SLOGAN.length;
+
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    if (options.length === 0) setOptions(shuffle(SLOGAN));
+  }, []);
+
+  useEffect(() => {
+    if (isCorrect && router) router.push("/handler/sign-in");
+  }, [isCorrect, router]);
+
+  const handleCheckAnswer = () => {
+    if (!isSentenceCompleted) return;
+
+    setIsCorrect(userAnswer.every((v, i) => SLOGAN[i] === v.value));
+  };
+
+  const handleOptionClick = (index: number) => {
+    const word = options.find((v) => v.index === index);
+    if (!word) return;
+
+    setOptions((prev) =>
+      prev.map((p) => ({
+        index: p.index,
+        value: p.index === index ? "" : p.value,
+      }))
+    );
+    setUserAnswer((prev) => [...prev, word]);
+  };
+
+  const handlePartClick = (index: number) => {
+    setIsCorrect(null);
+
+    const word = userAnswer.find((v) => v.index === index);
+    if (!word) return;
+
+    setUserAnswer((prev) => prev.filter((v) => v.index != index));
+    setOptions((prev) => prev.map((p) => (p.index === index ? word : p)));
+  };
+
+  return (
+    <div className="sm:max-w-3xl mx-4 sm:mx-auto">
+      <h2 className="font-semibold text-4xl sm:text-5xl my-8 leading-tight">
+        Learn by practice
+      </h2>
+      <div className="grid auto-rows-fr gap-6 mb-4">
+        <div className="relative h-full">
+          <div className="absolute -z-10 top-0 left-0 w-full h-full overflow-hidden **:border-b **:h-[53px] **:mb-[9px] sm:**:h-14 sm:**:mb-3.5">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+          <div className="flex gap-x-2 gap-y-4 sm:gap-y-6 flex-wrap">
+            {userAnswer.map((part) => (
+              <Button
+                key={part.index}
+                variant={"outline"}
+                tabIndex={0}
+                className="border rounded-xl"
+                onClick={() => handlePartClick(part.index)}
+              >
+                {part.value}
+              </Button>
+            ))}
+          </div>
+        </div>
+        <div className="flex gap-x-2 gap-y-4 sm:gap-y-6 flex-wrap justify-center sm:gap-x-4">
+          {options.map((option) => (
+            <Button
+              key={option.index}
+              variant={"outline"}
+              tabIndex={0}
+              className={cn(
+                "border rounded-xl mb-2",
+                option.value === "" &&
+                  "border-muted/50 shadow-muted/50 bg-muted/50"
+              )}
+              disabled={option.value === ""}
+              onClick={() => handleOptionClick(option.index)}
+            >
+              {option.value === "" ? (
+                <span className="invisible">
+                  {userAnswer.find((v) => v.index === option.index)?.value}
+                </span>
+              ) : (
+                option.value
+              )}
+            </Button>
+          ))}
+        </div>
+      </div>
+      <div>
+        <p className="font-semibold mb-3 text-secondary-dark">
+          {isCorrect !== null ? (
+            <>
+              {isCorrect === false
+                ? "You can do better, try again."
+                : "Perfect, Redirecting to learning mode."}
+            </>
+          ) : (
+            <>&nbsp;</>
+          )}
+        </p>
+        <Button
+          variant={"primary"}
+          disabled={!isSentenceCompleted}
+          onClick={handleCheckAnswer}
+          className="text-lg py-4 px-10"
+        >
+          {isSentenceCompleted
+            ? "Check Answer and start"
+            : "Solve quiz to start!"}
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default SloganBuilder;
--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,105 @@
 import { stackServerApp } from "@stack/server";
+import Image from "next/image";
 import Link from "next/link";
+import { buttonVariants } from "./components/ui/button";
+import SloganBuilder from "./SloganBuilder";
+import { cn } from "./utils/cn";
 
 export default async function WebsitePage() {
   const user = await stackServerApp.getUser();
 
   return (
-    <div>
-      <h1>Website</h1>
-      <p>
-        <Link href={"/learn"}>Start Learning</Link>
-      </p>
-      <p>
-        <Link href={"/dashboard"}>Admin Dashboard</Link>
-      </p>
-      {!user && (
-        <>
-          <Link href={`${process.env.NEXT_PUBLIC_AUTH_HANDLER_BASE}/sign-in`}>
-            Sign in
-          </Link>{" "}
-          -
-          <Link href={`${process.env.NEXT_PUBLIC_AUTH_HANDLER_BASE}/sign-up`}>
-            Sign up
-          </Link>
-        </>
-      )}
-      {user && (
-        <div>
-          {user?.displayName}{" "}
-          <Link href={`${process.env.NEXT_PUBLIC_AUTH_HANDLER_BASE}/sign-out`}>
-            Sign out
-          </Link>
+    <>
+      <header className="sticky top-0 bg-background w-full shadow-lg z-10">
+        <div className="container max-w-7xl px-2 py-4 mx-auto flex gap-3 items-center">
+          <h1 className="hidden sm:block">
+            <Link
+              href={`${process.env.NEXT_PUBLIC_APP_BASE}`}
+              className="flex gap-2 items-center hover:text-primary"
+            >
+              <Image
+                src="/assets/binesh-logo.svg"
+                width={50}
+                height={50}
+                alt="Binesh Logo"
+              />
+              <div className="text-3xl font-bold hidden sm:block">BINESH</div>
+            </Link>
+          </h1>
+          <nav className="grow">
+            <ul className="flex gap-1 justify-center text-lg">
+              <li>
+                <Link
+                  href="#features"
+                  className="block p-2 px-6 rounded-full hover:bg-muted"
+                >
+                  Features
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="#how-it-works"
+                  className="block p-2 px-6 rounded-full hover:bg-muted"
+                >
+                  How It Works
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="#about"
+                  className="block p-2 px-6 rounded-full hover:bg-muted"
+                >
+                  About
+                </Link>
+              </li>
+            </ul>
+          </nav>
+          <div className="flex gap-2 items-center">
+            <Link
+              href={`${process.env.NEXT_PUBLIC_AUTH_HANDLER_BASE}/sign-in`}
+              className={cn(
+                buttonVariants({ variant: "outline" }),
+                "border-muted-foreground shadow-none active:translate-0"
+              )}
+            >
+              Sign in
+            </Link>
+          </div>
         </div>
-      )}
-    </div>
+      </header>
+      <section className="text-center">
+        <SloganBuilder />
+      </section>
+      <div>
+        <h1>Website</h1>
+        <p>
+          <Link href={"/learn"}>Start Learning</Link>
+        </p>
+        <p>
+          <Link href={"/dashboard"}>Admin Dashboard</Link>
+        </p>
+        {!user && (
+          <>
+            <Link href={`${process.env.NEXT_PUBLIC_AUTH_HANDLER_BASE}/sign-in`}>
+              Sign in
+            </Link>{" "}
+            -
+            <Link href={`${process.env.NEXT_PUBLIC_AUTH_HANDLER_BASE}/sign-up`}>
+              Sign up
+            </Link>
+          </>
+        )}
+        {user && (
+          <div>
+            {user?.displayName}{" "}
+            <Link
+              href={`${process.env.NEXT_PUBLIC_AUTH_HANDLER_BASE}/sign-out`}
+            >
+              Sign out
+            </Link>
+          </div>
+        )}
+      </div>
+    </>
   );
 }
