bd27979e47981b401812861d2d446d0557314e56
Reza Zahedi <r.zahedi@gmail.com>
2026-01-08 13:53:50 -0800

    remove default dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default async function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn(inter.className, mdSerifDisplay.variable, "dark")}>
+      <body className={cn(inter.className, mdSerifDisplay.variable)}>
         <StackProvider app={stackServerApp}>
           <StackTheme>{children}</StackTheme>
         </StackProvider>
