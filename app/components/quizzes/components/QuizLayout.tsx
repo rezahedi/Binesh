99361dbaad6acf46acb79f66c0eaf848a980999a
Reza Zahedi <r.zahedi@gmail.com>
2026-01-31 10:09:17 -0800

    make content optional in quiz layout

--- app/components/quizzes/components/QuizLayout.tsx
+++ app/components/quizzes/components/QuizLayout.tsx
@@ -2,17 +2,17 @@ import { ReactNode } from "react";
 import ReactMarkdown from "@/lib/markdown";
 
 const QuizLayout = ({
-  content,
+  content = "",
   children,
 }: {
-  content: string;
+  content?: string;
   children: ReactNode;
 }) => {
   return (
     <>
       <div className="flex-10">
         <div className="my-4 p-6 sm:p-8 rounded-xl bg-card space-y-4">
-          <ReactMarkdown>{content}</ReactMarkdown>
+          {content && <ReactMarkdown>{content}</ReactMarkdown>}
           {children}
         </div>
       </div>
