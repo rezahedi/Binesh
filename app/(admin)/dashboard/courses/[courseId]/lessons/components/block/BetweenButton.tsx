0f08a0fa9f9e9d76e670743113f756c24199a40a
Reza Zahedi <r.zahedi@gmail.com>
2026-02-23 16:00:52 -0800

    refactor: improve style of add between button

--- app/(admin)/dashboard/courses/[courseId]/lessons/components/block/BetweenButton.tsx
+++ app/(admin)/dashboard/courses/[courseId]/lessons/components/block/BetweenButton.tsx
@@ -9,17 +9,17 @@ const BetweenButton = ({
   onAddStepAfter: (index: number) => void;
 }) => {
   return (
-    <div className="flex justify-center">
-      <Button
-        type="button"
-        variant="outline"
-        size="sm"
-        onClick={() => onAddStepAfter(index)}
-      >
-        <PlusIcon className="h-4 w-4" />
-        Add Step Here
-      </Button>
-    </div>
+    <button
+      type="button"
+      onClick={() => onAddStepAfter(index)}
+      className="group/between block w-full border border-transparent cursor-pointer"
+    >
+      <div className="flex justify-center border border-transparent h-0 w-1/3 mx-auto my-4 group-hover/between:w-full rounded transition-all duration-150 group-hover/between:border-muted-foreground">
+        <div className="-translate-y-1/2 bg-background border border-muted size-8 rounded-full flex items-center justify-center text-muted-foreground group-hover/between:bg-muted-foreground group-hover/between:border-muted-foreground group-hover/between:text-foreground">
+          <PlusIcon className="h-4 w-4" />
+        </div>
+      </div>
+    </button>
   );
 };
 
