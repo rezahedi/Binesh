6b5ee785b9d629ac0c4d96fee2b16e0b7b252684
Reza Zahedi <r.zahedi@gmail.com>
2026-02-24 16:24:35 -0800

    fix: transparent bg of add button to make less distraction

--- app/(admin)/dashboard/courses/[courseId]/lessons/components/block/TextareaBlock.tsx
+++ app/(admin)/dashboard/courses/[courseId]/lessons/components/block/TextareaBlock.tsx
@@ -17,7 +17,7 @@ const TextareaBlock = ({ id, label, value, ...props }: TextareaBlockProps) => {
         className="group/between block w-full border border-transparent cursor-pointer"
       >
         <div className="flex justify-center border border-transparent h-0 w-1/3 mx-auto my-4 group-hover/between:w-full rounded transition-all duration-150 group-hover/between:border-muted-foreground">
-          <div className="-translate-y-1/2 bg-background border border-muted size-8 rounded-full flex items-center justify-center text-muted-foreground group-hover/between:bg-muted-foreground group-hover/between:border-muted-foreground group-hover/between:text-foreground">
+          <div className="-translate-y-1/2 bg-transparent border border-muted size-8 rounded-full flex items-center justify-center text-muted-foreground group-hover/between:bg-muted-foreground group-hover/between:border-muted-foreground group-hover/between:text-foreground">
             <PlusIcon className="h-4 w-4" />
           </div>
         </div>
