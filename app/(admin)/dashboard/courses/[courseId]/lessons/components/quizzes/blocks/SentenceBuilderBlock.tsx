c7e302f937dafae6fc55853e8d9a70722fe2f7d2
Reza Zahedi <r.zahedi@gmail.com>
2026-02-27 13:03:35 -0800

    feat: enhance SentenceBuilderBlock to support special character insertion and improve option handling

--- app/(admin)/dashboard/courses/[courseId]/lessons/components/quizzes/blocks/SentenceBuilderBlock.tsx
+++ app/(admin)/dashboard/courses/[courseId]/lessons/components/quizzes/blocks/SentenceBuilderBlock.tsx
@@ -8,24 +8,51 @@ type SentenceBuilderBlockProps = {
   onChange: (next: SentenceBuilderQuizType) => void;
 };
 
-const joinOptions = (items: string[]) => items.join("|");
-const splitOptions = (value: string) => value.split("|");
+const SPECIAL_CHAR = "|"; //"\u00A0";
+const joinOptions = (items: string[]) => items.join(SPECIAL_CHAR);
+const splitOptions = (value: string) => value.split(SPECIAL_CHAR);
 
 const SentenceBuilderBlock = ({
   value,
   errors,
   onChange,
 }: SentenceBuilderBlockProps) => {
+  const specialCharListener = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.shiftKey && e.code === "Space") {
+      e.preventDefault();
+      const textarea = e.currentTarget;
+      const start = textarea.selectionStart;
+      const end = textarea.selectionEnd;
+      const newValue =
+        textarea.value.substring(0, start) +
+        SPECIAL_CHAR +
+        textarea.value.substring(end);
+      textarea.value = newValue;
+      // move cursor to after the inserted character
+      textarea.selectionStart = textarea.selectionEnd = start + 1;
+
+      // trigger change event
+      handleChange({
+        target: textarea,
+      } as React.ChangeEvent<HTMLTextAreaElement>);
+    }
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const inputValue = e.target.value;
+    const options = splitOptions(inputValue);
+    onChange({ ...value, options });
+  };
+
   return (
     <div className="space-y-3">
       <div>
         <TextareaBlock
-          label="Sentence Builder Content"
+          label="Sentence"
           rows={4}
           value={joinOptions(value.options)}
-          onChange={(e) =>
-            onChange({ ...value, options: splitOptions(e.target.value) })
-          }
+          onKeyDown={specialCharListener}
+          onChange={handleChange}
         />
         <p className="mt-1 text-xs text-muted-foreground">
           Type the correct sentence and split parts with |. Example: This is| my
