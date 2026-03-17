b1b4e0688ccf752096bbf173b8e72bc36b5cfe67
Reza Zahedi <r.zahedi@gmail.com>
2026-03-16 21:57:56 -0700

    feat: Improve LeverScale's panel config form

--- app/components/Interactive/LeverScale/LeverScale.config.tsx
+++ app/components/Interactive/LeverScale/LeverScale.config.tsx
@@ -1,4 +1,5 @@
 import { LeverScaleProps } from "@/components/Interactive/LeverScale";
+import { Input } from "@/components/ui/input";
 
 const LeverScaleConfig = ({
   props,
@@ -9,39 +10,63 @@ const LeverScaleConfig = ({
 }) => {
   if (!props) return null;
 
+  const onMassWeightChange = (index: number, value: number) => {
+    onChange({
+      ...props,
+      masses: props.masses.map((m, i) => (i === index ? [value, m[1]] : m)),
+    });
+  };
+
+  const onMassPositionChange = (index: number, value: number) => {
+    onChange({
+      ...props,
+      masses: props.masses.map((m, i) => (i === index ? [m[0], value] : m)),
+    });
+  };
+
   return (
     <div>
       <h3>Lever Scale Panel</h3>
-      {props.masses.map((m, i) => (
-        <div key={i}>
-          Masse {i + 1}:
-          <input
-            defaultValue={m[0]}
-            min={0}
-            max={10}
-            step={1}
-            type="range"
-          />{" "}
-          {m[0]}
-          <input
-            defaultValue={m[1]}
-            min={-Math.floor(props.rodLength / 2)}
-            max={Math.floor(props.rodLength / 2)}
-            step={1}
-            type="number"
-          />
-        </div>
-      ))}
+      <div className="flex gap-2">
+        {props.masses.map((m, i) => (
+          <div key={i}>
+            Masse {i + 1}:
+            <Input
+              defaultValue={m[0]}
+              min={0}
+              max={100}
+              step={5}
+              type="number"
+              onChange={(e) =>
+                onMassWeightChange(i, Number(e.currentTarget.value))
+              }
+            />
+            <Input
+              defaultValue={m[1]}
+              min={-props.fulcrum}
+              max={props.rodLength - props.fulcrum - 1}
+              step={1}
+              type="range"
+              onChange={(e) =>
+                onMassPositionChange(i, Number(e.currentTarget.value))
+              }
+            />
+          </div>
+        ))}
+      </div>
       Fulcrum:{" "}
-      <input
+      <Input
         defaultValue={props.fulcrum}
+        min={1}
+        max={props.rodLength - 2}
+        step={1}
         type="number"
         onChange={(e) =>
           onChange({ ...props, fulcrum: Number(e.currentTarget.value) })
         }
       />
       Rod Length:{" "}
-      <input
+      <Input
         defaultValue={props.rodLength}
         type="number"
         onChange={(e) =>
@@ -49,9 +74,12 @@ const LeverScaleConfig = ({
         }
       />
       Draggable Mass Index:{" "}
-      <input
+      <Input
         defaultValue={props.draggableWeightIndex}
         type="number"
+        min={-1}
+        max={props.masses.length - 1}
+        step={1}
         onChange={(e) =>
           onChange({
             ...props,
